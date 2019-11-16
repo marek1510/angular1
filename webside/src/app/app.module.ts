a45cd9bc901ed0ef15bee43fd62e416ee0227fba
marek1510 <marek.ilko@26house.com>
2019-11-16 13:38:51 +0100

    first commit

---
+++ webside/src/app/app.module.ts
@@ -0,0 +1,38 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { IntroComponent } from './intro/intro.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { ServicesComponent } from './services/services.component';
+import { TestimonialsComponent } from './testimonials/testimonials.component';
+import { ClientsComponent } from './clients/clients.component';
+import { PricingComponent } from './pricing/pricing.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { SocialComponent } from './social/social.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    IntroComponent,
+    GalleryComponent,
+    ServicesComponent,
+    TestimonialsComponent,
+    ClientsComponent,
+    PricingComponent,
+    FooterComponent,
+    HeaderComponent,
+    NavigationComponent,
+    SocialComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
